af3c7c5f23c190ad8b673038cdfe25d216c5181e
徐猛 <xumeng@123kanfang.com>
2025-03-17 07:33:19 +0800

    feat: 尝试添加canvas控制录制的帧数

--- src/utils/video-recorder.ts
+++ src/utils/video-recorder.ts
@@ -4,14 +4,41 @@ export class VideoRecorder {
   public videoElement: HTMLVideoElement
   public mediaRecorder: MediaRecorder | null
   private recordedChunks: any[]
-
+  aniId: number | undefined
+  canvasElement: HTMLCanvasElement | undefined
   constructor(videoElement: HTMLVideoElement) {
     this.videoElement = videoElement
     this.mediaRecorder = null
     this.recordedChunks = []
     console.log('[VideoRecorder]', this)
   }
-
+  useCanvasCapture() {
+    const that = this
+    const video = this.videoElement
+    const canvas = document.createElement('canvas');
+    this.canvasElement = canvas
+    const ctx = canvas.getContext('2d');
+    function captureFrame() {
+      // 设置 canvas 尺寸与视频相同
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      // 在 canvas 上绘制视频当前帧
+      ctx!.drawImage(video, 0, 0, canvas.width, canvas.height);
+      // 这里可以继续处理 canvas 上的内容，比如编码为视频流
+      // 使用 requestAnimationFrame 来尝试达到目标帧率
+      that.aniId = requestAnimationFrame(captureFrame);
+    }
+    // 开始捕获帧
+    captureFrame();
+  }
+  stopCanvasCapture() {
+    if (this.aniId) {
+      cancelAnimationFrame(this.aniId)
+    }
+    if (this.canvasElement) {
+      this.canvasElement.remove()
+    }
+  }
   start() {
     const stream = this.videoElement.srcObject
     this.mediaRecorder = new MediaRecorder(stream, {
